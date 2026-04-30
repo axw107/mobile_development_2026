9770b164a3667455359fbd69ce895e30f9c8d0e5
Tyoma Gorbunov <bivaetctoskazat123@gmail.com>
2026-04-30 14:39:14 +0500

    Удалено лишнее

--- front_exercise2/src/main/java/com/example/front_exercise2/MainActivity.java
+++ front_exercise2/src/main/java/com/example/front_exercise2/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(new GameView(this));
         setContentView(R.layout.activity_main);
     }
 
